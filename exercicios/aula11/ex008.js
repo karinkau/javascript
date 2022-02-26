7ca835b1fd608174ea89a0f13b3c413e2e98d88b
Karine Rodrigues <karinkau13@gmail.com>
2022-02-26 19:28:16 -0300

    Aula 11 - Condições  26-02-2022
    Aula 11 - condições - parte 1. Incluídos os exercícios 8 e 9 feitos em arquivos js, ou seja, em scripts puros.

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,13 @@
+/* ======================
+                AULA 11 - ex008
+    ======================
+*/
+var vel = 78
+
+console.log(`A velocidade do carro é ${vel} km/h.`)
+
+//Condição simples não tem else 
+if (vel > 60) {
+    console.log(`MULTADO! Você ultrapassou a velocidade permitida.`)
+}
+console.log(`Use sempre cinto de segurança!`)
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,20 @@
+/* ======================
+                AULA 11 - ex009
+    ======================
+*/
+var pais = 'EUA'
+
+console.log(`Vivendo em ${pais}`)
+
+//Condição composta
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
+
+/*
+    Com a extensão NODE.js Exerc 
+    basta pressionar F8 no teclaado 
+    para EXECUTAR 
+*/
