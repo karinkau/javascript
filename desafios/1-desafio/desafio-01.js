adbd71623551906157803fee08edb34cfdc35c54
Karine Rodrigues <karinkau13@gmail.com>
2022-02-27 14:43:47 -0300

    Desafios em JS  27-02-2022
    Inserido pasta de desafios e todos os 13 pdfs dos 13 desafios a serem realizados durante o curso.

---
+++ desafios/1-desafio/desafio-01.js
@@ -0,0 +1,16 @@
+//  FUNÇÕES EM JAVASCRIPT
+
+function um(){
+    window.alert(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n     VOCÊ CLICOU NO 1º  BOTÃO \n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)}
+ 
+
+   
+ 
+function dois(){
+    window.alert(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n     VOCÊ CLICOU NO 2º  BOTÃO \n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)}
+ 
+   
+
+function tres(){
+    window.alert(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n     VOCÊ CLICOU NO 3º  BOTÃO \n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)}
+   
