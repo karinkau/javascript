9f14dfac35db5f3ff18a3dd72ed03fc8c0ddf443
Karine Rodrigues <karinkau13@gmail.com>
2022-03-01 17:02:02 -0300

    Desafio 01_  01-03-2022
    Alterações pontuais e inclusão do desafio 01.

--- desafios/1-desafio/desafio-01.js
+++ desafios/1-desafio/desafio-01.js
@@ -1,16 +1,15 @@
-//  FUNÇÕES EM JAVASCRIPT
+//DESAFIO 01 - Curso de Javascript
 
 function um(){
-    window.alert(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n     VOCÊ CLICOU NO 1º  BOTÃO \n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)}
- 
+    window.alert(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n     ☺ VOCÊ CLICOU NO 1º  BOTÃO ☺ \n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)
+}
 
-   
- 
+
 function dois(){
-    window.alert(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n     VOCÊ CLICOU NO 2º  BOTÃO \n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)}
- 
+    window.alert(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n     ☺ VOCÊ CLICOU NO 2º  BOTÃO ☺ \n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)
+}
    
-
 function tres(){
-    window.alert(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n     VOCÊ CLICOU NO 3º  BOTÃO \n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)}
+    window.alert(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n     ☺ VOCÊ CLICOU NO 3º  BOTÃO ☺ \n\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)
+}
    
