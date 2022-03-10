30840c5b508a9ddc88682b2c10be05694679a726
Karine Rodrigues <karinkau13@gmail.com>
2022-03-10 14:45:03 -0300

    Desafio 02  10-03-2022
    Resolução do desafio nº2 com arquivos em css, js e pdf.

---
+++ desafios/2-desafio/desafio-02.js
@@ -0,0 +1,8 @@
+// Desafio 02 - Curso em Javascript com o prof.Gustavo Guanabara.
+
+function responda(){
+    var nome = window.prompt('▬ Qual é o seu nome?')  
+    var anos = window.prompt(`▬ Olá! ${nome}! Quantos anos você tem?`)
+
+    window.alert(`\n\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ \n\n ☺ Acabei de conhecer ${nome}, que tem ${anos} anos. ☺ \n\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n`)
+}
